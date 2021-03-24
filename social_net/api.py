4113303f03e5b0a0d864ab6dbd5b8e562d7ac506
chernikovanv <52738292+chernikovanv@users.noreply.github.com>
2021-03-24 16:43:15 +0300

    Update api.py

--- social_net/api.py
+++ social_net/api.py
@@ -1,5 +1,10 @@
-from social_net import app
+import os
+from flask import Flask, jsonify
+app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return 'Hello world!'
+@app.route('/')
+def welcome():
+    return jsonify({'status': 'api working'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))
