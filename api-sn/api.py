84ed462ca6ce17ce7e6e8eb27e3d52eea68c1644
chernikovanv <52738292+chernikovanv@users.noreply.github.com>
2021-03-26 22:19:07 +0300

    Update api.py

--- api-sn/api.py
+++ api-sn/api.py
@@ -5,6 +5,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import LoginManager
 from flask_login import UserMixin
 from flask_login import login_user
+from flask_login import login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 import mysql.connector
 
@@ -97,8 +98,9 @@ def index():
     return render_template('index.html')
 
 @main.route('/profile')
+@login_required
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html',name=current_user.email)
 
 auth = Blueprint('auth', __name__)
 
